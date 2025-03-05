9f15e7010909489fb0d5e1f095f9f88b70bd07ab
Fake4091 <aswends7517@gmail.com>
2025-03-05 12:55:26 -0600

    Initial Commit

---
+++ src/main/java/com/example/Unit_Project/UnitProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.Unit_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnitProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Unit_Project/UnitProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Unit_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
