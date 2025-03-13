6799f0f1da81bddda6ecdb3ae2314f9ae342b30e
Chris Skeen <chrisskeen2006@gmail.com>
2025-03-13 15:41:42 -0500

    added most of the Plane stuff

--- src/main/java/com/example/Unit_Project/Plane/PlaneController.java
+++ src/main/java/com/example/Unit_Project/Plane/PlaneController.java
@@ -1,9 +1,56 @@
 package com.example.Unit_Project.Plane;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.Unit_Project.Airport.Airport;
+import org.apache.coyote.Request;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/planes")
 public class PlaneController {
+    // Getting ahold of repository and service --- ↓
+    private final PlaneService planeService;
+    private final PlaneRepository planeRepository;
+
+    @Autowired
+    public PlaneController(PlaneService planeService, PlaneRepository planeRepository) {
+        this.planeService = planeService;
+        this.planeRepository = planeRepository;
+    }
+    // Getting ahold of repository and service --- ↑
+
+    // Read Data (ALL) --- ↓
+
+    @GetMapping
+    public List<Plane> getAllPlanes() {
+        return planeService.getAllPlanes();
+    }
+
+    // Read Data (ONE) --- ↓
+
+    @GetMapping(path="{id}")
+    public Plane getPlaneById(@PathVariable Long id) {
+        return planeService.getPlaneById(id);
+    }
+
+    // Upload Data () --- ↓
+    @PostMapping
+    public Plane createPlane(@RequestBody Plane plane) {
+        return planeService.createPlane(plane);
+    }
+
+    // Update Data () --- ↓
+    @PutMapping("update/{id}")
+    public Plane updatePlane(@PathVariable Long id, @RequestBody Plane newPlane) {
+        return planeService.updatePlane(id, newPlane);
+    }
+
+    // Delete Data () --- ↓
+    @DeleteMapping("delete/{id}")
+    public void deletePlane(@PathVariable Long id) {
+        planeService.deleteById(id);
+    }
+
 }
--- src/main/java/com/example/Unit_Project/Plane/PlaneService.java
+++ src/main/java/com/example/Unit_Project/Plane/PlaneService.java
@@ -1,7 +1,70 @@
 package com.example.Unit_Project.Plane;
 
+import java.util.List;
+import java.util.Optional;
+
+import com.example.Unit_Project.Airport.Airport;
+import com.example.Unit_Project.Airport.AirportRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Service
 public class PlaneService {
+
+    // Setting up repository --- ↓
+
+    private static PlaneRepository planeRepository = null;
+    private final AirportRepository airportRepository;
+
+    @Autowired
+    public PlaneService(PlaneRepository planeRepository, AirportRepository airportRepository) {
+        this.planeRepository = planeRepository;
+        this.airportRepository = airportRepository;
+    }
+
+    // Setting up repository --- ↑
+
+
+    public List<Plane> getAllPlanes(){
+
+        return planeRepository.findAll();
+    }
+
+    public Plane getPlaneById(Long id) {
+
+        Optional<Plane> planeOptional = planeRepository.findById(id);
+        if (planeOptional.isEmpty()) {
+            throw new IllegalStateException("Plane with id " + id + " not found");
+        }
+        return planeOptional.get();
+
+    }
+
+    public Plane createPlane(Plane plane) {
+        List<Plane> planesWithId = planeRepository.getPlaneById(plane.getId());
+        if(!planesWithId.isEmpty()){
+            throw new IllegalStateException("Plane with ID " + plane.getId() + " already exists");
+        }
+        return planeRepository.save(plane);
+    }
+
+    public Plane updatePlane(Long id, Plane newPlane) {
+        List<Plane> planesWithId = planeRepository.getPlaneById(newPlane.getId());
+        if(planesWithId.isEmpty()) {
+            throw new IllegalStateException("Plane with ID " + newPlane.getId() + " doesn't exist!");
+        }
+
+        Optional<Plane> planeOptional = planeRepository.findById(id);
+        Plane plane = planeOptional.get();
+
+        plane.setModel(newPlane.getModel());
+        plane.setNumberOfSeats(newPlane.getNumberOfSeats());
+
+        return planeRepository.save(plane);
+    }
+
+
+    public void deleteById(Long id) {
+        planeRepository.deleteById(id);
+    }
 }
